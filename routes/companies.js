41d906dd109c50c2740fdc1cc104d1ec93f9c717
Connor Dieck <connordieck@gmail.com>
2021-10-23 17:31:58 -0400

    initial setup

---
+++ routes/companies.js
@@ -0,0 +1,18 @@
+/** Routes for companies of express-biztime */
+
+const express = require("express");
+const ExpressError = require("../expressError");
+const router = express.Router();
+const db = require("../db");
+
+// Return list of companies
+router.get("/", async (req, res, next) => {
+	try {
+		const results = await db.query(`SELECT * FROM companies`);
+		return res.json({ companies: results.rows });
+	} catch (e) {
+		return next(e);
+	}
+});
+
+module.exports = router;
