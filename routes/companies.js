432df0a2eebc5e2d7471754b5632287e4bfbe2d5
Connor Dieck <connordieck@gmail.com>
2021-10-25 17:16:20 -0400

    finished company routes

--- routes/companies.js
+++ routes/companies.js
@@ -15,4 +15,60 @@ router.get("/", async (req, res, next) => {
 	}
 });
 
+// Return obj of company
+router.get("/:code", async (req, res, next) => {
+	try {
+		const { code } = req.params;
+		const results = await db.query(`SELECT * FROM companies WHERE code=$1`, [ code ]);
+		if (results.rows.length === 0) {
+			throw new ExpressError(`Can't find company with code ${code}`, 404);
+		}
+		return res.send({ company: results.rows[0] });
+	} catch (e) {
+		return next(e);
+	}
+});
+
+// Adds a company
+router.post("/", async (req, res, next) => {
+	try {
+		const { code, name, description } = req.body;
+		const results = await db.query(
+			"INSERT INTO companies (code, name, description) values ($1, $2, $3) RETURNING code, name, description",
+			[ code, name, description ]
+		);
+		return res.status(201).json({ company: results.rows[0] });
+	} catch (e) {
+		return next(e);
+	}
+});
+
+// Edit existing company
+router.put("/:code", async (req, res, next) => {
+	try {
+		const { code } = req.params;
+		const { name, description } = req.body;
+		const results = await db.query(
+			"UPDATE companies SET name=$1, description=$2 WHERE code=$3 RETURNING code, name, description",
+			[ name, description, code ]
+		);
+		if (results.rows === 0) {
+			throw new ExpressError(`Can't find company with ${code}`, 404);
+		}
+		return res.send({ company: results.rows[0] });
+	} catch (e) {
+		return next(e);
+	}
+});
+
+// Deletes company
+router.delete("/:code", async (req, res, next) => {
+	try {
+		const results = db.query("DELETE FROM companies WHERE code = $1", [ req.params.code ]);
+		return res.send({ msg: "DELETED!" });
+	} catch (e) {
+		return next(e);
+	}
+});
+
 module.exports = router;
